001bb25b9777a17ecf23ef4249ae2a1853d9cf90
문지원 <81554184+jeewonMoon@users.noreply.github.com>
2025-04-01 12:21:40 +0900

    design: 전역 스타일링 적용 (#4)
    * design: 전역 스타일링 적용
    
    - styled-components, styled-reset을 추가했습니다.
    - 색상, 사이즈 테마를 설정했습니다.
    - body와 main에 너비와 padding, margin 값을 지정했습니다.
    
    * design: 디자인 요소 변경
    
    - 배경색과 글씨의 기본색을 지정했습니다.
    - main의 border-radius와 배경색을 지정했습니다.

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,50 @@
+import {createGlobalStyle} from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+    ${reset};
+
+    *{
+        box-sizing: border-box;
+    }
+    html {
+        font-size: 62.5%;
+        display: flex;
+        justify-content: center;
+    }
+    body {
+        font-family: 'Noto Sans KR';
+        width: 44rem;
+        background-color: #F3F4F6;
+        color: #030712;
+    } 
+    input {
+        outline: none;
+        padding: 0;
+    }
+    button {
+        cursor: pointer;
+        outline: none;
+        background: none;
+        border: none;
+        padding: 0;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+    main {
+        margin: 1.6rem;
+        padding: 1.6rem;
+        border-radius: 8px;
+        background-color: #fff;
+    }
+
+    @media screen and (max-width: 440px) {
+        body {
+            width: 100%;
+        }
+    }
+`;
+
+export default GlobalStyle;
