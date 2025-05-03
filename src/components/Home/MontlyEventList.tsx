efad0950a2c7e2440bfaeabf6e51d4da8b913258
subin1215 <subum6302@gmail.com>
2025-05-03 10:53:49 +0900

    refactor: 이번달 행사 목록 스타일 수정

--- src/components/Home/MontlyEventList.tsx
+++ src/components/Home/MontlyEventList.tsx
@@ -48,6 +48,8 @@ MonthlyEventList.displayName = 'MonthlyEventList';
 const MonthlyEventWrap = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  width: 100%;
   gap: 1rem;
 `;
 
